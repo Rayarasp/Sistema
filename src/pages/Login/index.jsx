93d5a16627e73f79358f774b718bea3731114e3e
Rayarasp <rayaraps93@gmail.com>
2025-05-02 11:41:07 -0300

    sistema1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,12 @@
-export default function LoginPage() {
-    return(
-        <p> Aqui é a página de Login</p>
-    )
-  
-  }
+import svg from "/public/anima.svg"
+
+export default function LoginPage(){
+    return (
+        <div className="w-full h-screen flex">
+            <div className="w-[40%] h-full flex items-center justify-center"></div>
+            <div className="w-[60%] h-full bg-[#6366EE] flex items-center justify-center rounded-l-lg">
+                <img src={svg} alt="imagem-animada" className="" />
+            </div>
+        </div>
+    ) 
+}
